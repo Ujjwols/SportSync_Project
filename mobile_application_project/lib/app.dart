bb15bfe3d4db4b01bd2d88c159faf67cc5e45e4a
Ujjwols <shrestha.ujjwol1@gmail.com>
2024-12-12 20:06:57 +0545

    registration desgin

--- mobile_application_project/lib/app.dart
+++ mobile_application_project/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_application_project/view/Registarionview,dart';
 import 'package:mobile_application_project/view/loginview.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginView(),
+      home: RegistrationView(),
     );
   }
 }
